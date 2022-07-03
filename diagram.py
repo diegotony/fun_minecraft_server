7b274683ceaf89972ee6b86dcdcfa3e4534cb435
Diego Castillo <diegotony1396@gmail.com>
2022-07-02 23:30:33 -0500

    fix: diagrams build

--- diagram.py
+++ diagram.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # diagram.py
-from diagram import Diagram
-from diagrams.aws.compute import EC2,EC2Instance, Lambda
+from diagrams import Diagram
+from diagrams.aws.compute import EC2, Lambda
 from diagrams.aws.management import CloudwatchRule
 
 with Diagram("fun_minecraft_server",filename="diagram", show=False, direction="TB"):
